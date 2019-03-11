6d608ca33f7b998f6443728607d747ad650edd4b
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-03-11 17:31:41 +0100

    Updates

---
+++ precache-manifest.da3d3e7534c13ad3b1ba0a8c2ed6b990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8789d6bb6dac684f74",
+    "url": "/todoapp/static/js/runtime~main.ca8789d6.js"
+  },
+  {
+    "revision": "ad4c3e0915dd4e78fcb0",
+    "url": "/todoapp/static/js/main.ad4c3e09.chunk.js"
+  },
+  {
+    "revision": "899ec8aec8c7f4ec3c59",
+    "url": "/todoapp/static/js/1.899ec8ae.chunk.js"
+  },
+  {
+    "revision": "ad4c3e0915dd4e78fcb0",
+    "url": "/todoapp/static/css/main.de17146c.chunk.css"
+  },
+  {
+    "revision": "2db9dea5d2cd461fb32a5d93975f18fd",
+    "url": "/todoapp/index.html"
+  }
+];
