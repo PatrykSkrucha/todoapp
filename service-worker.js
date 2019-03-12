8238eeb3ed0dcb517df6e4487bd4513a18f474f3
PatrykSkrucha <patryk.skrucha@gmail.com>
2019-03-12 14:18:34 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todoapp/precache-manifest.da3d3e7534c13ad3b1ba0a8c2ed6b990.js"
+  "/todoapp/precache-manifest.fa6cb2e3e4e46d1d08761dd4f52e2fe1.js"
 );
 
 workbox.clientsClaim();
